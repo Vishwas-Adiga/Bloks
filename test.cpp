e7872c0d1cdf8ddf6a49e72c8ab309fd4dfcbc54
Vishwas Adiga <vishwasadiga@gmail.com>
2017-09-26 21:37:24 +0530

    Revert "Interpreter PATH"
    This reverts commit d2845b85712c1d7f87765a9bf671ca9bc2267a9a.

--- test.cpp
+++ test.cpp
@@ -5,12 +5,10 @@
 using namespace std;
 
 int main(){
-    system("/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6 /Users/Thiru/Downloads/pytest.py");
-    system("/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6 /Users/Thiru/Downloads/pytest.py");
+    system("python pytest.py");
+    system("python pytest.py");
     cout << "Opening pytest";
-    
-    
-    return 0;
+    getch();
+	
+return 0;
 }
-
-/*I had to do this cause i have 3 python interpreters.. this is different on Unix like os and windows right????? But we will being doing it for Windows (i know). Just so that you know that we might need to specify the path of the interpreter. i have read that it's not need in windows.... but idk if it will work in our school*/
