d2845b85712c1d7f87765a9bf671ca9bc2267a9a
Pritheshwar <pritheshwar@gmail.com>
2017-09-26 21:29:04 +0530

    Interpreter PATH
    Hey vishwas,
    just check it out

--- test.cpp
+++ test.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 int main(){
-    system("python pytest.py");
-    system("python pytest.py");
+    system("/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6 /Users/Thiru/Downloads/pytest.py");
+    system("/Library/Frameworks/Python.framework/Versions/3.6/bin/python3.6 /Users/Thiru/Downloads/pytest.py");
     cout << "Opening pytest";
-    getch();
-	
-return 0;
+    
+    
+    return 0;
 }
+
+/*I had to do this cause i have 3 python interpreters.. this is different on Unix like os and windows right????? But we will being doing it for Windows (i know). Just so that you know that we might need to specify the path of the interpreter. i have read that it's not need in windows.... but idk if it will work in our school*/
